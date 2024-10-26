ea6226c18ee56e0f45cc4a09f30dceb6d6333664
wispl <wispl.8qbkk@slmail.me>
2024-10-26 18:36:14 -0400

    feat: add map drawing code

---
+++ filter_data.m
@@ -0,0 +1,12 @@
+routes = readtable("routes.xlsx");
+stops = readtable("stops.txt");
+
+% find nb stops only
+new_brunswick = routes(routes.Municipality == "NEW BRUNSWICK",:);
+ids = new_brunswick.StopID_StopNum;
+nb_stops = stops(ismember(stops.stop_code, ids), :);
+
+% write file as json
+file = fopen("stops.json","w");
+fprintf(file,"%s",jsonencode(nb_stops));
+fclose(file);
